725b24448b331612bbc5b1ce369282531b5c16b6
davuggdeat <119799176+davuggdeat@users.noreply.github.com>
2026-06-14 20:26:59 -0300

    Update Partido.java

--- Mundial/src/Mundial/Partido.java
+++ Mundial/src/Mundial/Partido.java
@@ -1,49 +1,64 @@
 package Mundial;
 
-public abstract class Partido implements Jugable {
-	
-	private String equipoLocal;
-	private String equipoVisitante;
-	private String fecha;
-	private String estadio;
-	private int golesLocal;
-	private int golesVisitante;
-	
-	// Contructor
-	public partidos(String equipoLocal, String equipoVisitante, String fecha, String estadio) {
-		this.equipoLocal = equipoLocal;
-		this.equipoVisitante = equipoVisitante;
-		this.fecha = fecha;
-		this.estadio = estadio;
-		this.golesLocal = 0;
-		this.golesVisitante = 0;
-	}
-	
-	// Métodos para cargar el resultado del partido
-	
-	@Override
-    private void jugarPartidos() {
-		//Resultado de ejemplo
-		golesLocal = (int) (Math.random() *5);
-		golesVisitante = (int) (Math.random() * 5);
-	}
+public class Partido implements Jugable {
 
-	//Metodo para mostrar el resultado 
-	@Override
-	public void mostrarResultados () {
-		System.out.println("--------------------------------------");
-		System.out.println("partido del mundial 2026");
-		System.out.println("Fecha: " + fecha);
-		System.out.println("Estadio: " + estadio);
-		System.out.println(equipoLocal + " " + golesLocal +  " - " + golesVisitante + " " + equipoVisitante);
-		
-		if (golesLocal > golesVisitante) {
-			System.out.println("Ganador: " + equipoLocal);
-		} else if (golesVisitante > golesLocal) {
-			System.out.println("Ganador: " + equipoVisitante);
-		} else { 
-			System.out.println("Resultado: Empate");
-			
-		}	
-	}
+    private String equipoLocal;
+    private String equipoVisitante;
+    private String fecha;
+    private String estadio;
+    private int golesLocal;
+    private int golesVisitante;
+
+    public Partido(String equipoLocal, String equipoVisitante, String fecha, String estadio) {
+
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+        this.fecha = fecha;
+        this.estadio = estadio;
+    }
+
+    @Override
+    public void jugarPartido() {
+
+        golesLocal = (int) (Math.random() * 5);
+        golesVisitante = (int) (Math.random() * 5);
+    }
+
+    @Override
+    public void mostrarResultados() {
+
+        System.out.println("--------------------------------------");
+        System.out.println("PARTIDO DEL MUNDIAL 2026");
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Estadio: " + estadio);
+
+        System.out.println(equipoLocal + " " + golesLocal +  " - " + golesVisitante + " " + equipoVisitante);
+
+        if (golesLocal > golesVisitante) {
+            System.out.println("Ganador: " + equipoLocal);
+        } else if (golesVisitante > golesLocal) {
+            System.out.println("Ganador: " + equipoVisitante);
+        } else {
+            System.out.println("Resultado: Empate");
+        }
+    }
+
+    public String getGanador() {
+
+        if (golesLocal > golesVisitante)
+            return equipoLocal;
+
+        if (golesVisitante > golesLocal)
+            return equipoVisitante;
+
+        return "Empate";
+    }
+
+    public String getEquipoLocal() {
+        return equipoLocal;
+    }
+
+    public String getEquipoVisitante() {
+        return equipoVisitante;
+    }
 }
